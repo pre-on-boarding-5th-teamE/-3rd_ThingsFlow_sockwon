d5b81d3723bbf89cc40d3b7ea2a4b324714c6c87
lee sock won <lsw4uto@gmail.com>
2022-11-04 15:27:19 +0900

    init

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+// const routes = require("./routes");
+
+const createApp = () => {
+  const app = express();
+  app.use(express.json());
+  app.use(cors());
+  app.use(
+    morgan(":method :url :status :res[content-length] - :response-time ms")
+  );
+
+  app.get("/ping", (req, res, next) => {
+    res.status(200).json({ message: "pong" });
+    next();
+  });
+
+  //   app.use(routes);
+  return app;
+};
+
+module.exports = { createApp };
